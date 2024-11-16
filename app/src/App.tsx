ba150b1b9844ec55af9c0a8357ba3a8a75ef2e6a
Carlos Veríssimo <carlosverissimo3001@gmail.com>
2024-11-16 19:24:08 +0000

    chore(fe): initial config

---
+++ app/src/App.tsx
@@ -0,0 +1,39 @@
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { AuthProvider } from './contexts/AuthContext'
+import { LoginPage } from './pages/LoginPage'
+import { DashboardPage } from './pages/DashboardPage'
+import { Navbar } from './components/Navbar'
+import { ProtectedRoute } from './components/misc/ProtectedRoute'
+import { useDarkMode } from './hooks/useDarkMode'
+// import { ListView } from '.'
+
+function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+  const {isDark} = useDarkMode();
+
+  return (
+    <AuthProvider>
+        <div className="min-h-screen bg-gray-50">
+          {!isLoginPage && <Navbar />}
+          <main>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <DashboardPage />
+                  </ProtectedRoute>
+                }
+              />
+              {/* <Route path="/dashboard/:id" element={<ListView />} /> */}
+              <Route path="/" element={<Navigate to="/login" replace />} />
+            </Routes>
+          </main>
+        </div>
+      </AuthProvider>
+  )
+}
+
+export default App
